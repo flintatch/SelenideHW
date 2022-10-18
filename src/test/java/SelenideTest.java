283c2f2fb8efded8c859631fabeae8fda425b303
gvlad <gvladovsky@gmail.com>
2022-10-18 23:00:36 +0300

    added new search test

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -1,8 +1,7 @@
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exist;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -12,7 +11,9 @@ public class SelenideTest {
     public void softAssertionsTest() {
         open("https://github.com/selenide/selenide");
         $("#wiki-tab").click();
-        $(byText("Soft assertions")).click();
-        $(byText("3. Using JUnit5 extend test class:")).should(exist);
+        $(".Box--condensed").$("js-wiki-more-pages-link").click();
+        $(".Box--condensed").shouldHave(text("SoftAssertions"));
+        $(".Box--condensed").$(byText("SoftAssertions"));
+        $(".markdown-body").$(byText("3. Using JUnit5 extend test class:")).shouldBe(visible);
     }
 }
